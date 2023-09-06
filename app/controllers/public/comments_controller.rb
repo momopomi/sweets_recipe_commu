c635474eb1624bfe9015aecdebcdd87c2edd531d
EC2 Default User <132799867+momopomi@users.noreply.github.com>
2023-09-06 14:20:16 +0000

    add review

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -20,6 +20,6 @@ class Public::CommentsController < ApplicationController
    private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :review)
   end
 end
