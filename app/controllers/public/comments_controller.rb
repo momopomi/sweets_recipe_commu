4166832d97ce1960f78fb906d34684470a71106c
EC2 Default User <132799867+momopomi@users.noreply.github.com>
2023-09-07 16:18:36 +0000

    edit comment and review

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,6 +6,7 @@ class Public::CommentsController < ApplicationController
     recipe = Recipe.find(params[:recipe_id])
     comment = current_customer.comments.new(comment_params)
     comment.recipe_id = recipe.id
+    
     comment.save
     redirect_to recipe_path(recipe)
   end
