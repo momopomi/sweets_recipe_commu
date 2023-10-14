85002cef1af5b69044e5aa88d974b16e5010b170
EC2 Default User <132799867+momopomi@users.noreply.github.com>
2023-10-14 12:35:55 +0000

    new deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: "momo@gmail.com",
-  password: "anzu4712"
+  email: "momo@g.com",
+  password: "anzu1234"
 )
